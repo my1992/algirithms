3ebb8f0d211aeb552e8b9dfa5d581bd0511680d5
my1992 <tjtjwmy@qq.com>
2017-02-13 10:43:58 +0800

    查找一个数据的最长递增子集

--- lcs.py
+++ lcs.py
@@ -1,3 +1,6 @@
+import numpy as np
+
+
 def lcs(s1, s2):
     """
     查找两个字符串的最长公共子串
@@ -30,6 +33,38 @@ def lcs(s1, s2):
 
     sorted_result = sorted(len_dict.items(), key=lambda t: t[1], reverse=True)
     return sorted_result
+
+
+def increasing_son_series(data):
+    """
+    查找一个数据的最长递增子集
+    :param data:
+    :return:
+    """
+    f = np.zeros(len(data))  # 存放当前索引位置上以它为七点可能存在的最长子序列个数
+    max_index = -1
+    max_len = -1
+
+    for i in range(len(data)-1, -1, -1):
+        f[i] = 1
+        if i == len(data) - 1:
+            continue
+        for j in range(i+1, len(data)):
+            if data[i] < data[j] and f[i] <= f[j]:
+                f[i] = f[j] + 1
+        if f[i] > max_len:
+            max_len = f[i]
+            max_index = i
+
+    for i in range(max_index, len(data)):
+        if f[i] == max_len:
+            print(data[i])
+            max_len -= 1
+
+
+# if __name__ == '__main__':
+#     data = [3, 18, 7, 14, 10, 12, 23, 41, 16, 24]
+#     increasing_son_series(data)
 
 if __name__ == '__main__':
     s1 = 'ascdgfawe'
